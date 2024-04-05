63a3db21bfbd93a1d6acccb421863b7bb434fc2c
codyvaibhav <166171200+codyvaibhav@users.noreply.github.com>
2024-04-06 02:15:52 +0530

    Recursions 2

---
+++ R2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <string>
+using namespace std;
+
+int main(){
+    string str = "";
+    int n=3;
+    for(int i=1;i<=n;i++){
+            str += to_string(i);}
+    cout<<str;      
+
+}
